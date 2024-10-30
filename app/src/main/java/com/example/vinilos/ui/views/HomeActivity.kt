b61d6dc20c080c8851c1a1fc196fdb2304dd48f1
Sergio Augusto Celis Esteban <s.celise@uniandes.edu.co>
2024-10-30 10:52:27 -0500

    Se creó la barra de navegación

--- app/src/main/java/com/example/vinilos/ui/views/HomeActivity.kt
+++ app/src/main/java/com/example/vinilos/ui/views/HomeActivity.kt
@@ -1,12 +1,29 @@
 package com.example.vinilos.ui.views
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.vinilos.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val visitorButton = findViewById<Button>(R.id.visitorButton)
+        val collectorButton = findViewById<Button>(R.id.collectorButton)
+
+        val intent = Intent(this, MainActivity::class.java)
+
+        visitorButton.setOnClickListener {
+            startActivity(intent)
+        }
+
+        collectorButton.setOnClickListener {
+            startActivity(intent)
+        }
+
     }
 }
