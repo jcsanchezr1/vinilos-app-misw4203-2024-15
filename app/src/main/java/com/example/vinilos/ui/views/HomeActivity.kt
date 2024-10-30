0e3881f739993a9dfc3da9300d0cfa64dba1d9b8
Sergio Augusto Celis Esteban <s.celise@uniandes.edu.co>
2024-10-29 20:06:20 -0500

    Version inicial

---
+++ app/src/main/java/com/example/vinilos/ui/views/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vinilos.ui.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.vinilos.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
