ff618b71c2b074942e0c1ff4238ea9cf5c244939
Sergio Augusto Celis Esteban <s.celise@uniandes.edu.co>
2024-10-31 22:04:38 -0500

    Se crea la vista del listado de álbumes

--- app/src/main/java/com/example/vinilos/ui/views/AlbumFragment.kt
+++ app/src/main/java/com/example/vinilos/ui/views/AlbumFragment.kt
@@ -1,17 +1,33 @@
 package com.example.vinilos.ui.views
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.example.vinilos.R
 
 class AlbumFragment : Fragment() {
+
+    private lateinit var ivLogout: ImageView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.album_fragment, container, false)
+
+        val view = inflater.inflate(R.layout.album_fragment, container, false)
+
+        ivLogout = view.findViewById(R.id.ivLogout)
+
+        ivLogout.setOnClickListener {
+            val intent = Intent(requireActivity(), HomeActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            requireActivity().finish()
+        }
+
+        return view
     }
 }
