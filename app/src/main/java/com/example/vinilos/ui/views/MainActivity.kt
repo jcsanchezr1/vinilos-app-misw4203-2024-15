b9fe3bc661669bc2351fae68c3f969582ad8dda2
scelise <s.celise@uniandes.edu.co>
2024-11-06 16:22:32 -0500

    Se corrigió el bug de cerrar sesión en Artistas (#19)

--- app/src/main/java/com/example/vinilos/ui/views/MainActivity.kt
+++ app/src/main/java/com/example/vinilos/ui/views/MainActivity.kt
@@ -1,31 +1,48 @@
 package com.example.vinilos.ui.views
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.vinilos.R
+import com.example.vinilos.databinding.ActivityMainBinding
+import com.example.vinilos.ui.viewmodels.HeaderViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var headerViewModel: HeaderViewModel
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        headerViewModel = ViewModelProvider(this).get(HeaderViewModel::class.java)
+        binding.headerLayout.header = headerViewModel
+        binding.headerLayout.lifecycleOwner = this
 
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.artists -> {
-                    loadFragment(ArtistFragment())
-                    true
-                }
 
                 R.id.albums -> {
+                    headerViewModel.setTitleAndAddButtonVisibility("Álbumes", true)
                     loadFragment(AlbumFragment())
                     true
                 }
 
+                R.id.artists -> {
+                    headerViewModel.setTitleAndAddButtonVisibility("Artistas", false)
+                    loadFragment(ArtistFragment())
+                    true
+                }
+
                 R.id.collectors -> {
+                    headerViewModel.setTitleAndAddButtonVisibility("Coleccionistas", false)
                     true
                 }
 
@@ -33,7 +50,15 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        headerViewModel.setTitleAndAddButtonVisibility("Álbumes", true)
         loadFragment(AlbumFragment())
+
+        binding.headerLayout.ivLogout.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+            finish()
+        }
     }
 
     private fun loadFragment(fragment: Fragment) {
